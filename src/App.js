c509c0e3b430b160459a6808f13eff64d5133c57
Vamberto Jr <juniorwan2012@gmail.com>
2024-11-18 18:13:09 -0300

    feat: create react commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const handleClick = () => {
+    alert('Olá, mundo!');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Bem-vindo!</h1>
+      <button onClick={handleClick}>Clique aqui</button>
+    </>
   );
 }
 
